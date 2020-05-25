eb6da7a643342638732b2006027da3f885ff63f8
JR Cologne <kontakt@jr-cologne.de>
2020-05-25 22:27:48 +0200

    Implement simple API authorization
    Implement simple API authorization in order to prevent misuse of the API by other parties and not by devCommunity.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
 use DevCommunityDE\CodeFormatter\CodeFormatterApp;
+use DevCommunityDE\CodeFormatter\Api\Auth\ApiAuthorizer;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$auth = new ApiAuthorizer;
+$auth->authorize();
 
 $app = new CodeFormatterApp;
 $app->run();
