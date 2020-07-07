e57d4128f2d536833423bcf227997ff6d0b3ae20
asccc <git@putc.de>
2020-07-07 23:04:38 +0200

    fix indention of phan config

--- .phan/config.php
+++ .phan/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-  'target_php_version' => '7.4',
-  'directory_list' => [
-    'app',
-    'vendor',
-  ],
-  'exclude_analysis_directory_list' => [
-    'vendor',
-  ],
+    'target_php_version' => '7.4',
+    'directory_list' => [
+        'app',
+        'vendor',
+    ],
+    'exclude_analysis_directory_list' => [
+        'vendor',
+    ],
 ];
